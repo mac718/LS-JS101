f18ce7dbfb06eff494b721fe9b88c036b279aa64
Mike Coon <mac718@gmail.com>
2021-06-08 20:00:10 -0700

    initial version with no input validations

--- lesson_2/loanCalculator.js
+++ lesson_2/loanCalculator.js
@@ -7,3 +7,22 @@ function prompt(message) {
 console.log("******************************************");
 console.log("* Hello! Welcome to the Loan Calculator! *");
 console.log("******************************************");
+
+prompt("Enter the amount of the loan:");
+let loanAmount = readline.question();
+
+prompt("Enter the Annual Percentage Rate (APR):");
+let apr = readline.question();
+
+prompt("Enter the loan duration:");
+let loanDuration = readline.question();
+
+let monthlyInterestRate = apr / 100 / 12;
+let durationInMonths = loanDuration * 12;
+
+let monthlyPayment =
+  loanAmount *
+  (monthlyInterestRate /
+    (1 - Math.pow(1 + monthlyInterestRate, -durationInMonths)));
+
+prompt(`Your monthly payment is: $${monthlyPayment}`);
