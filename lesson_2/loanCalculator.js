da4172ac9b32b4c79fbc2394dbb0e8354d83f73b
Mike Coon <mac718@gmail.com>
2021-06-09 19:49:57 -0700

    add prompt/loop for additional loan calculations

--- lesson_2/loanCalculator.js
+++ lesson_2/loanCalculator.js
@@ -21,36 +21,52 @@ function invalidInput(number) {
 
 greet();
 
-prompt("Enter the amount of the loan without the dollar sign ($):");
-let loanAmount = readline.question();
+while (true) {
+  prompt("Enter the amount of the loan without the dollar sign ($):");
+  let loanAmount = readline.question();
 
-while (invalidInput(loanAmount)) {
-  prompt("Hmm... that is not a valid loan amount. Please try again:");
-  loanAmount = readline.question();
+  while (invalidInput(loanAmount)) {
+    prompt("Hmm... that is not a valid loan amount. Please try again:");
+    loanAmount = readline.question();
+  }
+
+  prompt(
+    "Enter the Annual Percentage Rate (APR) without the percent sign (%):"
+  );
+  let apr = readline.question();
+
+  while (invalidInput(apr)) {
+    prompt("Hmm... that is not a valid loan amount. Please try again:");
+    apr = readline.question();
+  }
+
+  prompt("Enter the loan duration in years:");
+  let loanDurationInYears = readline.question();
+
+  while (invalidInput(loanDurationInYears)) {
+    prompt("Hmm... that is not a valid loan amount. Please try again:");
+    loanDurationInYears = readline.question();
+  }
+
+  let monthlyInterestRate = apr / 100 / 12;
+  let durationInMonths = loanDurationInYears * 12;
+
+  let monthlyPayment =
+    loanAmount *
+    (monthlyInterestRate /
+      (1 - Math.pow(1 + monthlyInterestRate, -durationInMonths)));
+
+  prompt(`Your monthly payment is: $${monthlyPayment.toFixed(2)}\n`);
+
+  prompt("Would you like to calculate another payment (y/n)?");
+  let anotherCalc = readline.question();
+
+  while (!["y", "n"].includes(anotherCalc.toLowerCase())) {
+    prompt("Please enter 'y' or 'n'.");
+    anotherCalc = readline.question();
+  }
+
+  if (anotherCalc === "n") {
+    break;
+  }
 }
-
-prompt("Enter the Annual Percentage Rate (APR) without the percent sign (%):");
-let apr = readline.question();
-
-while (invalidInput(apr)) {
-  prompt("Hmm... that is not a valid loan amount. Please try again:");
-  apr = readline.question();
-}
-
-prompt("Enter the loan duration in years:");
-let loanDurationInYears = readline.question();
-
-while (invalidInput(loanDurationInYears)) {
-  prompt("Hmm... that is not a valid loan amount. Please try again:");
-  loanDurationInYears = readline.question();
-}
-
-let monthlyInterestRate = apr / 100 / 12;
-let durationInMonths = loanDurationInYears * 12;
-
-let monthlyPayment =
-  loanAmount *
-  (monthlyInterestRate /
-    (1 - Math.pow(1 + monthlyInterestRate, -durationInMonths)));
-
-prompt(`Your monthly payment is: $${monthlyPayment}`);
