b93b07f2dad90116c9a36d031cd630e3df228111
Mike Coon <mac718@gmail.com>
2021-06-10 19:55:43 -0700

    wrap payment calculation and additional payment option in funtions in loanCalculator

--- lesson_2/loanCalculator.js
+++ lesson_2/loanCalculator.js
@@ -1,5 +1,9 @@
 const readline = require("readline-sync");
 
+let loanAmount;
+let apr;
+let loanDurationInYears;
+
 function printGreeting() {
   console.log("******************************************");
   console.log("* Hello! Welcome to the Loan Calculator! *");
@@ -14,51 +18,57 @@ function invalidInput(number) {
   let splitNum = number.split(".");
   return (
     Number.isNaN(Number(number)) ||
+    Number(number) < 1 ||
     number === "" ||
     splitNum.length > 2 ||
     (splitNum[1] && splitNum[1].length > 2)
   );
 }
 
-printGreeting();
-
-while (true) {
+function getLoanAmount() {
   prompt("Enter the amount of the loan without the dollar sign ($):");
-  let loanAmount = readline.question();
+  loanAmount = readline.question();
 
   while (invalidInput(loanAmount)) {
     prompt("Hmm... that is not a valid loan amount. Please try again:");
     loanAmount = readline.question();
   }
+}
 
+function getAPR() {
   prompt(
     "Enter the Annual Percentage Rate (APR) without the percent sign (%):"
   );
-  let apr = readline.question();
+  apr = readline.question();
 
   while (invalidInput(apr)) {
     prompt("Hmm... that is not a valid loan amount. Please try again:");
     apr = readline.question();
   }
+}
 
+function getLoanDurationInYears() {
   prompt("Enter the loan duration in years:");
-  let loanDurationInYears = readline.question();
+  loanDurationInYears = readline.question();
 
   while (invalidInput(loanDurationInYears)) {
     prompt("Hmm... that is not a valid loan amount. Please try again:");
     loanDurationInYears = readline.question();
   }
+}
 
+function calculateMonthlyPayment(loanAmount, apr, loanDurationInYears) {
   let monthlyInterestRate = apr / 100 / 12;
   let durationInMonths = loanDurationInYears * 12;
 
-  let monthlyPayment =
+  return (
     loanAmount *
     (monthlyInterestRate /
-      (1 - Math.pow(1 + monthlyInterestRate, -durationInMonths)));
+      (1 - Math.pow(1 + monthlyInterestRate, -durationInMonths)))
+  );
+}
 
-  prompt(`Your monthly payment is: $${monthlyPayment.toFixed(2)}\n`);
-
+function offerOtherCalculation() {
   prompt("Would you like to calculate another payment (y/n)?");
   let anotherCalc = readline.question();
 
@@ -67,7 +77,27 @@ while (true) {
     anotherCalc = readline.question();
   }
 
-  if (anotherCalc === "n") {
+  return anotherCalc;
+}
+
+printGreeting();
+
+while (true) {
+  getLoanAmount();
+
+  getAPR();
+
+  getLoanDurationInYears();
+
+  let monthlyPayment = calculateMonthlyPayment(
+    loanAmount,
+    apr,
+    loanDurationInYears
+  );
+
+  prompt(`Your monthly payment is: $${monthlyPayment.toFixed(2)}\n`);
+
+  if (offerOtherCalculation() === "n") {
     break;
   }
 }
