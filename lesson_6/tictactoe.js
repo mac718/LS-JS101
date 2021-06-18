f96fa86a288a0a64f11884a32747093fd044366a
Mike Coon <mac718@gmail.com>
2021-06-18 10:16:35 -0700

    create game loop and boardFull function in ttt

--- lesson_6/tictactoe.js
+++ lesson_6/tictactoe.js
@@ -1,4 +1,7 @@
 const readline = require("readline-sync");
+const INITIAL_MARKER = " ";
+const HUMAN_MARKER = "X";
+const COMPUTER_MARKER = "O";
 
 function displayBoard(board) {
   console.log("");
@@ -19,7 +22,7 @@ function initializeBoard() {
   let board = {};
 
   for (let square = 1; square <= 9; square++) {
-    board[square] = " ";
+    board[square] = INITIAL_MARKER;
   }
 
   return board;
@@ -29,27 +32,50 @@ function prompt(message) {
   console.log(`=> ${message}`);
 }
 
+function emptySquares(board) {
+  return Object.keys(board).filter((key) => board[key] === INITIAL_MARKER);
+}
+
 function playerChoosesSquare(board) {
   let square;
 
-  let emptySquares = Object.keys(board).filter((key) => board[key] === " ");
-
   while (true) {
-    prompt("Select the square you would like to mark (1-9):");
+    prompt(
+      `Select the square you would like to mark (${emptySquares(board).join(
+        ", "
+      )}):`
+    );
     square = readline.question();
 
-    if (emptySquares.includes(square)) {
-      break;
-    } else {
-      prompt("Sorry, that is not a valid choice.");
-    }
+    if (emptySquares(board).includes(square)) break;
+
+    prompt("Sorry, that is not a valid choice.");
   }
 
-  board[square] = "X";
+  board[square] = HUMAN_MARKER;
+}
+
+function computerChoosesSquare(board) {
+  let randomIndex = Math.floor(Math.random() * emptySquares(board).length);
+
+  board[emptySquares(board)[randomIndex]] = COMPUTER_MARKER;
+}
+
+function boardFull(board) {
+  return emptySquares(board).length === 0;
+}
+
+function someoneWon(board) {
+  return false;
 }
 
 let board = initializeBoard();
+displayBoard(board);
 
-displayBoard(board);
-playerChoosesSquare(board);
-displayBoard(board);
+while (true) {
+  playerChoosesSquare(board);
+  computerChoosesSquare(board);
+  displayBoard(board);
+
+  if (someoneWon(board) || boardFull(board)) break;
+}
